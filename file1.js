4b7928594ff6e40ad1fa8478bc52052b9f517740
TonmoyZohani <tonmoyzohani@gmail.com>
2025-11-21 23:12:49 +0600

    file 1 fixed

--- file1.js
+++ file1.js
@@ -1,7 +1,9 @@
 const { a: x } = require("./file2.js");
 const { a: y } = require("./file3.js");
 
-const { add } = require("./utils/add.js");
-const { sub } = require("./utils/sub.js");
+// const { add } = require("./utils/add.js");
+// const { sub } = require("./utils/sub.js");
+const { add, sub } = require("./utils");
 
 console.log(add(x, y));
+console.log(sub(y, x));
