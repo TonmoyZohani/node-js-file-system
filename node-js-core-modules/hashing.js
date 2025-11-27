1007e0306a493b4e3191bff3239d293a20e90a67
TonmoyZohani <tonmoyzohani@gmail.com>
2025-11-27 19:00:43 +0600

    hashing js

--- node-js-core-modules/hashing.js
+++ node-js-core-modules/hashing.js
@@ -6,3 +6,17 @@ const md5Hash2 = crypto.createHash("md5").update("password123").digest("hex");
 console.log("input: password123");
 console.log("MD5 HashedPassword: ", md5Hash);
 console.log("MD5 Hash2 : ", md5Hash2);
+
+const sha256Hash = crypto
+  .createHash("sha256")
+  .update("password123")
+  .digest("hex");
+console.log("input: password123");
+console.log("SHA256 HashedPassword: ", sha256Hash);
+
+const sha512Hash = crypto
+  .createHash("sha512")
+  .update("password123")
+  .digest("hex");
+console.log("input: password123");
+console.log("SHA512 HashedPassword: ", sha512Hash);
